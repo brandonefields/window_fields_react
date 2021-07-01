765aabf122775aa1748ea46479fd0a6cc6ebf35d
BrandonFields <brandonefields@gmail.com>
2021-07-01 17:25:15 -0600

    cleared out default react stuff and changed app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img 
+      src="https://github.com/brandonefields/window_fields_react/blob/main/Untitled_Artwork.png?raw=true" 
+      alt="image of logo" height="500" ></img>
     </div>
   );
 }

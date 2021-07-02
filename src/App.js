103bc0d7308c9b583a9196c28ee1666169498ae9
BrandonFields <brandonefields@gmail.com>
2021-07-01 21:14:23 -0600

    cleared out the css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
 
 function App() {
+
+ 
+
   return (
     <div className="App">
+      <h1>Window Fields</h1>
       <img 
+      className="logo"
       src="https://github.com/brandonefields/window_fields_react/blob/main/Untitled_Artwork.png?raw=true" 
       alt="image of logo" height="500" ></img>
+      <h1>Window Fields</h1>
+      
     </div>
+    
   );
 }
 
